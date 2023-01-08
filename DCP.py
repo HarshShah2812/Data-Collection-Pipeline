0f2c39331bf92bc500f380cc688c85041aa8018c
HarshShah2812 <Harsh.Shah2812@outlook.com>
2023-01-08 22:49:00 +0000

    'modified'

--- DCP.py
+++ DCP.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import StaleElementReferenceException
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.firefox.options import Options
 import time
 from datetime import date, datetime
 import os
@@ -22,7 +23,22 @@ class Scraper:
             downward_scroller: scrolls to a specific set of coordinates when called
             get_products_page: finds the page element corresponding to the specific products section and clicks on it to go to the section
         '''
-        self.driver = webdriver.Firefox()
+        options = Options()
+        options.add_argument('--headless')
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument('--ignore-certificate-errors')
+        options.add_argument('--allow-running-insecure-content')
+        options.add_argument("--disable-extensions")
+        options.add_argument("--proxy-server='direct://'")
+        options.add_argument("--proxy-bypass-list=*")
+        options.add_argument("--start-maximized")
+        options.add_argument('--disable-gpu')
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--no-sandbox')
+        options.add_argument("--disable-notifications")
+        options.add_argument("--disable-infobars")
+        
+        self.driver = webdriver.Firefox(options = options)
         self.url = "https://urushop.co.uk/"
         self.get_request = self.driver.get(self.url)
         self._load_and_accept_cookies()
