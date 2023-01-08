edd3ca771535308c45a37e6dc8167495a1a88536
HarshShah2812 <Harsh.Shah2812@outlook.com>
2023-01-08 18:21:01 +0000

    modified

--- DCP.py
+++ DCP.py
@@ -1,6 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import StaleElementReferenceException
 from selenium.common.exceptions import NoSuchElementException
